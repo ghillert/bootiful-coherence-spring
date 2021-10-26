a1c0aaf38afbf6f64c9808a24ee4ffb02f15e0e6
Gunnar Hillert <gunnar@hillert.com>
2021-10-26 13:19:03 -1000

    Generate Spring Boot Starter App

---
+++ src/main/java/com/oracle/coherence/examples/todo/server/TodoListSpringServerApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.coherence.examples.todo.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListSpringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListSpringServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oracle/coherence/examples/todo/server/TodoListSpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.coherence.examples.todo.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListSpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
